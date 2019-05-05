876192c9c6c199a48fb02821d5ec9f98a4add253
chuiizeet <chuy.guerra19@outlook.es>
2019-05-04 19:57:17 -0500

    SWReveal

---
+++ smack/Smack/Smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by imac on 5/4/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - CGFloat(60)
+
+       
+    }
+    
+
+}
---
+++ smack/Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by imac on 5/4/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
+        self.view.addGestureRecognizer((self.revealViewController()?.tapGestureRecognizer())!)
+        
+
+ 
+    }
+    
+
+}
