fa70fc01c7631610ddd15e9db5d89d6e84c46cfe
chuiizeet <chuy.guerra19@outlook.es>
2019-05-06 17:41:19 -0500

    Creating a user

--- smack/Smack/Smack/Services/AuthService.swift
+++ smack/Smack/Smack/Services/AuthService.swift
@@ -15,7 +15,8 @@ class AuthService {
     static let instance = AuthService()
     
     let defaults = UserDefaults.standard
-    var isLogged : Bool {
+    
+    var isLoggedIn : Bool {
         get {
             return defaults.bool(forKey: LOGGED_IN_KEY)
         }
@@ -24,7 +25,7 @@ class AuthService {
         }
     }
     
-    var authToken : String {
+    var authToken: String {
         get {
             return defaults.value(forKey: TOKEN_KEY) as! String
         }
@@ -33,7 +34,7 @@ class AuthService {
         }
     }
     
-    var userEmail : String {
+    var userEmail: String {
         get {
             return defaults.value(forKey: USER_EMAIL) as! String
         }
@@ -65,38 +66,68 @@ class AuthService {
     func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
         
         let lowerCaseEmail = email.lowercased()
+        
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON {(response) in
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
             
             if response.result.error == nil {
+                guard let data = response.data else { return }
+                let json = JSON(data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
                 
-//                guard let data = response.data else { return }
-//                let json = JSON(data)
-//                self.userEmail = json["user"].stringValue
-//                self.authToken = json["token"].stringValue
-                
-                if let json = response.result.value as? Dictionary<String, Any> {
-                    if let email = json["user"] as? String {
-                        self.userEmail = email
-                    }
-                    if let token = json["token"] as? String {
-                        self.authToken = token
-                    }
-                }
-                
-                self.isLogged = true
+                self.isLoggedIn = true
                 completion(true)
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler) {
         
+        let lowerCaseEmail = email.lowercased()
         
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        
+        let header = [
+            "Authorization":"Bearer \(AuthService.instance.authToken)",
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        debugPrint(header)
+        
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+
+                let json = JSON(data)
+                let id = json["_id"].stringValue
+                let color = json["avatarColor"].stringValue
+                let avatarName = json["avatarName"].stringValue
+                let email = json["email"].stringValue
+                let name = json["name"].stringValue
+
+                
+                UserDataService.instance.setUserData(id: id, color: color, avatarName: avatarName, email: email, name: name)
+                completion(true)
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
     }
     
 }
