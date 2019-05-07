ec055f48fea55e6aed0a36c3280118f5e8e5bc6f
chuiizeet <chuy.guerra19@outlook.es>
2019-05-07 17:47:01 -0500

    LoggedIn UI

--- smack/Smack/Smack/Controller/ChannelVC.swift
+++ smack/Smack/Smack/Controller/ChannelVC.swift
@@ -10,20 +10,30 @@ import UIKit
 
 class ChannelVC: UIViewController {
     
-    
     @IBOutlet weak var logginBtn: UIButton!
+    @IBOutlet weak var userImg: CircleImage!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - CGFloat(60)
-
-       
+        NotificationCenter.default.addObserver(self, selector: #selector(userDataChanged), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
+    @objc func userDataChanged(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            self.logginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            self.logginBtn.setTitle("Title", for: .normal)
+            userImg.image =  UIImage(named: "menuProfileIcon")
+        }
     }
     
 }
