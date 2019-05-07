d54ffd7a791ec3489c5ed4c2406d6800105198bd
chuiizeet <chuy.guerra19@outlook.es>
2019-05-07 00:33:56 -0500

    Avatar picker: select image

--- smack/Smack/Smack/Controller/CreateAccountVC.swift
+++ smack/Smack/Smack/Controller/CreateAccountVC.swift
@@ -24,6 +24,13 @@ class CreateAccountVC: UIViewController {
       
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
+    
     @IBAction func pickAvatarPressed(_ sender: Any) {
         
         performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
