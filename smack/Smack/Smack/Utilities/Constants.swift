20e26ff09c10256e44c7a9731884792ae05b312d
chuiizeet <chuy.guerra19@outlook.es>
2019-05-08 00:03:36 -0500

    Heroku integration

--- smack/Smack/Smack/Utilities/Constants.swift
+++ smack/Smack/Smack/Utilities/Constants.swift
@@ -11,7 +11,8 @@ import Foundation
 typealias CompletionHandler = (_ Success: Bool) -> ()
 
 //EndP
-let BASE_URL = "http://localhost:3005/v1/"
+//let BASE_URL = "http://localhost:3005/v1/"
+let BASE_URL = "https://smackcool.herokuapp.com/v1/"
 
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
