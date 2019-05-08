558b9f5dc08b4c963d3b51e448fea45d5c4d6c8e
chuiizeet <chuy.guerra19@outlook.es>
2019-05-07 19:11:49 -0500

    Profile view

--- smack/Smack/Smack/Controller/ChannelVC.swift
+++ smack/Smack/Smack/Controller/ChannelVC.swift
@@ -22,7 +22,16 @@ class ChannelVC: UIViewController {
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        
+        if AuthService.instance.isLoggedIn {
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
+        
+        
     }
     
     @objc func userDataChanged(_ notif: Notification) {
