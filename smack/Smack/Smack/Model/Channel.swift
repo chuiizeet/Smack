bbfc28a4400028a3420075c49ca6b341c830555f
chuiizeet <chuy.guerra19@outlook.es>
2019-05-08 20:38:01 -0500

    Getting channels

---
+++ smack/Smack/Smack/Model/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by imac on 5/8/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+}
